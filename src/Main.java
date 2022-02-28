0b6468bab090b5af576d3b91b8f4daf41c9e5554
Roger Holden <r.holden@chi.ac.uk>
2022-02-28 19:41:20 +0000

    'fix' on Roger branch.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 
+/**
+ * Student work : Dan Chivers
+ */
 public class Main {
+
+    static ArrayList<Player> thePlayer = new ArrayList<>();
+
+
     public static void main(String args[]) {
 
 //        Scanner obj = new Scanner(System.in);
@@ -8,7 +15,14 @@ public class Main {
 //        int numberPlayers = obj.nextInt();
 //        System.out.println(numberPlayers);
 
-        ArrayList players = Player.createPlayers();
-        Player player = players.get(1);
+//        ArrayList players = Player.createPlayers();
+        thePlayer = Player.createPlayers();
+
+//        System.out.println( "class " + thePlayer.get(1).getFirstName() + " " + thePlayer.get(1).getLastName());
+
+
+        thePlayer.forEach((n) -> System.out.println(n.getFirstName() + " " + n.getLastName()   ));
+
     }
 }
+
